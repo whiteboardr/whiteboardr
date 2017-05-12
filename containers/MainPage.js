fbd7d8ccbf8654000455877be4625e7b55a64a1d
luisgreg99 <luisram90_101@hotmail.com>
2017-05-12 12:34:38 -0700

    added splitter

--- containers/MainPage.js
+++ containers/MainPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../actions';
-import { Page } from 'react-onsenui';
+import { Splitter, SplitterSide, SplitterContent, Page, List, ListItem } from 'react-onsenui';
 import NavBar from '../components/NavBar';
 import AddImage from './AddImage';
 import LoginDialog from './LoginDialog';
@@ -22,14 +22,39 @@ const MainPage = ({ logged_in, actions, navigator }) => {
     );
   }
   return (
-    <Page renderToolbar={() => <NavBar title='Whiteboarder' navigator={navigator} />}>
+    <Splitter>
+      <SplitterSide
+        style={{
+          boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
+        }}
+          side='left'
+          width={200}
+          collapse={false}
+          isSwipeable={true}
+          isOpen={this.state.isOpen}
+          onClose={this.setState({isOpen: false})}
+          onOpen={this.setState({isOpen: true})}
+      >
+          <Page>
+            <List
+              dataSource={['Profile', 'Followers', 'Settings']}
+            >
+              <ListItem tappable>Test</ListItem>
+            </List>
+          </Page>
+      </SplitterSide>
+      <SplitterContent>
+        <Page renderToolbar={() => <NavBar title='Whiteboarder' navigator={navigator} />}>
       {content}
-    </Page>
+        </Page>
+      </SplitterContent>
+    </Splitter>
   );
 };
 
 const mapStateToProps = (state) => ({
-  logged_in: state.settings.logged_in
+  logged_in: state.settings.logged_in,
+  isOpen: false
 });
 
 const mapDispatchToProps = (dispatch) => ({
