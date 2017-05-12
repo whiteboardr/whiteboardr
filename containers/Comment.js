9f6ac6233cc6d69fba275e27205c7fecab8998b8
Josh Gum <josh.gum@oregonstate.edu>
2017-05-12 12:12:32 -0700

    basic bullshit is barely working

--- containers/Comment.js
+++ containers/Comment.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../actions';
 
-import {ListItem} from 'react-onsenui';
+import { ListItem } from 'react-onsenui';
 
-const Comment = ({text, navigator}) => {
+const Comment = ({ navigator, id, title, text }) => {
   return (
     <ListItem tappable>
       <div> {text} </div>
@@ -13,13 +13,10 @@ const Comment = ({text, navigator}) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-});
-
 const mapDispatchToProps = (dispatch) => {
   return {
     actions: bindActionCreators(Actions, dispatch)
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Comment);
+export default connect(null, mapDispatchToProps)(Comment);
--- reducers/index.js
+++ reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import settings from './settings';
 import dialog from './dialog';
 import images from './images';
+import comments from './comments';
 
 const rootReducer = combineReducers({
   settings,
   images,
+  comments,
   dialog
 });
 
