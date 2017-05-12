195714041a08f370eb9c3626d668b88137d654fc
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-05-12 10:40:31 -0700

    Wires up Comments and Comment List

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import settings from './settings';
 import dialog from './dialog';
+import comments from './comments';
 
 const rootReducer = combineReducers({
   settings,
-  dialog
+  dialog,
+  comments
 });
 
 export default rootReducer;
